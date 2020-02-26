f05d843ae04a4517401d24e6d2a22631656c4467
Kyle Riner <rinerkyle97@gmail.com>
2020-02-26 21:28:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto] = "test"
 
      
 programmer_hash[:yukihiro_matsumoto] => {
